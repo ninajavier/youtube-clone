fea3d6dc6878714f398709264d4bf2220ea21e68
SamuelBAlba <brayanbalba24@gmail.com>
2023-03-12 11:52:34 -0400

    main

--- src/Components/common/Home.js
+++ src/Components/common/Home.js
@@ -1,4 +1,18 @@
+import
 
 export default function Home() {
+
+
+    return (
+        <div>
+          <input
+            type="text"
+            value={searchQuery}
+          />
+          <button>Search</button>
+        </div>
+      );
+    
+
 
 }
