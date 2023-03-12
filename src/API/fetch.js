7648f86fc94077365341c52c1c7328ca6bfaa7b7
SamuelBAlba <brayanbalba24@gmail.com>
2023-03-12 14:47:28 -0400

    changes

--- src/API/fetch.js
+++ src/API/fetch.js
@@ -1,3 +1,3 @@
 export function getAllVideos(searchTerm) {
-    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=`).then((response) => response.json())
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=AIzaSyAx57IX5X24MyPXeqkir7fzwCSlV_wnyBg`).then((response) => response.json())
 }
--- src/Components/common/Home.js
+++ src/Components/common/Home.js
@@ -3,47 +3,21 @@ import { getAllVideos } from "../../API/fetch";
 
 
 
-export default function Home() {
+export default function Home () {
     const [searchTerm, setSearchTerm] = useState("");
     const [videos, setVideos] = useState([]);
-    // const [allVideos, setAllVideos] = useState([]);
 
-
-    // function handleSubmit(event) {
-    //     const title = event.target.value;
-    //     const result = title.length ? filterVideos(title, allVideos) : allVideos;
-        
-    //     setSearchTerm(title);
-    //     setVideos(result);
-    //   }
-
-
-
-    function handleSearch () {
-        getAllVideos(searchTerm)
-
-    }
-
-    console.log(handleSearch)
-
-    // console.log(searchTerm)
-
+    
 
   return (
     <div>
-      <form>
-      <input
-        type="text"
-        value={searchTerm}
-        id="searchTitle"
-        placeholder="Search..."
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <input 
-      type="submit"
-      onClick={() => handleSearch()}
-      />
+      
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Search..." id="searchTitle" value={searchTerm} />
+        <button type="submit">Search</button>
+
       </form>
+    
 
 
       <section className="videos-index">
