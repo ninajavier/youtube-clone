c3c81e7f5dfc13ad79cef243b45589671711f433
SamuelBAlba <brayanbalba24@gmail.com>
2023-03-12 16:39:46 -0400

    react router

--- src/Components/common/Navbar.js
+++ src/Components/common/Navbar.js
@@ -8,10 +8,7 @@ export default function Navbar() {
         <h2>Youtube</h2>
 
         <nav>
-            {/* home will be a link */}
             <NavLink to="/"><h3>Home</h3></NavLink>
-            
-            {/* about will be a link */}
             <NavLink to="/about"><h3>About</h3></NavLink>
         </nav>
     </header>
