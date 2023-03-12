0b6aa31e643bbeb83e789901730958c0d54018a4
ninajavier <ninajavier13@gmail.com>
2023-03-12 11:29:33 -0400

    Started working on search functionality

--- src/Components/common/Home.js
+++ src/Components/common/Home.js
@@ -1,4 +1,49 @@
+import { useState } from "react";
+
+
 
 export default function Home() {
+    const [searchTerm, setSearchTerm] = useState("");
+    const [videos, setVideos] = useState([]);
+    const [allVideos, setAllVideos] = useState([]);
 
+
+    function handleSubmit(event) {
+        const title = event.target.value;
+        const result = title.length ? filterVideos(title, allVideos) : allVideos;
+        
+        setSearchTerm(title);
+        setVideos(result);
+      }
+
+      function filterVideos(search, videos) {
+        return videos.filter((video) => {
+          return video.items.snippet.title.toLowerCase().match(search.toLowerCase());
+        })
+      }
+
+
+  return (
+    <div>
+      <form>
+      <input
+        type="text"
+        value={searchTerm}
+        id="searchTitle"
+        placeholder="Search..."
+      />
+      <input 
+      type="submit"
+      onSumbit={handleSubmit}
+      />
+      </form>
+
+
+      <section className="videos-index">
+        {/* {videos.map((video) => {
+          return <VideoListing video={video} key={video.id} />;
+        })} */}
+      </section>
+    </div>
+  );
 }
