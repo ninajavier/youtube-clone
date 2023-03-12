7552028b00f033607f1349a704602b25a79e84f3
ninajavier <ninajavier13@gmail.com>
2023-03-12 14:46:20 -0400

    Semi functional search bar

--- src/API/fetch.js
+++ src/API/fetch.js
@@ -1,3 +1,3 @@
 export function getAllVideos(searchTerm) {
-    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=`).then((response) => response.json())
+    return fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=AIzaSyAiAM4YfR9rlIqsR47JTCsFnnYR4BgqJG4`).then((response) => response.json())
 }
--- src/Components/common/Home.js
+++ src/Components/common/Home.js
@@ -1,65 +1,42 @@
 import { useState } from "react";
-
+// import { getAllVideos } from "../../API/fetch";
 
 
 export default function Home() {
-    const [searchTerm, setSearchTerm] = useState("");
-    const [videos, setVideos] = useState([]);
-    const [allVideos, setAllVideos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-    // function handleSubmit(event) {
-    //     const title = event.target.value;
-    //     const result = title.length ? filterVideos(title, allVideos) : allVideos;
-        
-    //     setSearchTerm(title);
-    //     setVideos(result);
-    //   }
+    const apiKey = "AIzaSyAiAM4YfR9rlIqsR47JTCsFnnYR4BgqJG4";
+    const url = `https://youtube.googleapis.com/youtube/v3/search?q=${searchTerm}&part=snippet&maxResults=10&key=${apiKey}`;
 
+    const response = await fetch(url);
+    const videos = await response.json();
 
-
-    function handleSearch () {
-        getAllVideos(searchTerm)
-
-    }
-
-    console.log(handleSearch)
-
-    // console.log(searchTerm)
-
-      useEffect(() => {
-        getAllVideos().then((response) => {
-          setVideos(response);
-          setAllVideos(response);
-          setLoadingError(false);
-        }).catch((error) => {
-          setLoadingError(true);
-        })
-      }, [])
-
+    setSearchResults(videos.items);
+    console.log(searchResults)
+  }
 
   return (
     <div>
-      <form>
-      <input
-        type="text"
-        value={searchTerm}
-        id="searchTitle"
-        placeholder="Search..."
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <input 
-      type="submit"
-      onClick={() => handleSearch()}
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          id="searchTitle"
+          placeholder="Search..."
+          onChange={(event) => setSearchTerm(event.target.value)}
+        />
+        <button type="submit">Search</button>
       </form>
 
-
-      <section className="videos-index">
-        {/* {videos.map((video) => {
+      {/* <section className="videos-index">
+        {searchResults.map((video) => {
           return <VideoListing video={video} key={video.id} />;
-        })} */}
-      </section>
+        })}
+      </section> */}
     </div>
   );
 }
