59a62e26ec137e2ac1a5ab56271025fd53900609
ninajavier <ninajavier13@gmail.com>
2023-03-13 21:03:17 -0400

    behind the scenes work

--- src/Components/Youtube/VideoListing.js
+++ src/Components/Youtube/VideoListing.js
@@ -1,9 +1,22 @@
-import YouTube from 'react-youtube';
 import React from 'react';
+import YouTube from 'react-youtube';
+import { useParams } from 'react-router-dom';
 
+const VideoListing = () => {
+  const { id } = useParams();
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-export default function VideoListing() {
-    <>
-    <p>hello this worked</p>
-    </>
+  function _onReady(event) {
+    event.target.pauseVideo();
+  }
+
+  return <YouTube videoId={id} opts={opts} onReady={_onReady} />;
 }
+
+export default VideoListing;
