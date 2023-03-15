dc004cdf613ba044df0484cc080ae1c925522ba6
SamuelBAlba <brayanbalba24@gmail.com>
2023-03-14 20:10:39 -0400

    working on about

--- src/Components/common/About.js
+++ src/Components/common/About.js
@@ -1,11 +1,40 @@
+import Nina from '../../Images/Nina.J.jpeg'
+import Samuel from '../../Images/Samuel.B.Alba.jpeg'
+import GitLogo from '../../Images/GitHub-Mark.png'
+
+
+
 export default function About() {
 
     return (
        <div>
-        <a href="https://github.com/SamuelBAlba">
-            <img height="80px" alt="" src="../../"/>
 
+        <div className="description">
+
+
+        <h1>Project Description:</h1>
+        <p>Using React, one of Javascripts most powerful libraries, we were able to successfully create our very own YouTube front end clone. We integrated YouTube’s Data API on our platform to allow the user access to data from the videos searched and also the ability to watch a particular video.</p>
+
+        </div>
+
+        <div className="team">
+
+        <h2>The Team:</h2>
+
+        <img height='200px' src={Nina} alt='nina'/>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae atque, nobis obcaecati ad vel provident cupiditate deleniti delectus tednndwdd dw dw dwdwi jnwjddnjwdnj</p>
+       
+        <a href="https://github.com/ninajavier">
+            <img height="100px" src={GitLogo}/>
         </a>
+
+        <img height='200px' src={Samuel} alt='Samuel'/>
+
+
+        </div>
+
+       
 
        </div>
     )
